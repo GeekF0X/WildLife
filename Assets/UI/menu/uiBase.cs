b5d928d89b4429cdcd5b21fdedb0fefb911e5017
GeekF0X <carolinacastrocolaco@gmail.com>
2026-04-19 12:53:23 -0300

    Atualização da UI 1
    Mudei um pouco a UI

--- Assets/UI/menu/uiBase.cs
+++ Assets/UI/menu/uiBase.cs
@@ -1,40 +1,53 @@
 using System;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class uiBase : MonoBehaviour
 {
-    public GameObject configuraçao, principal, play, pause;
+    public GameObject configuracao, principal, play, pause;
+
+    public void Sandbox()
+    {
+        SceneManager.LoadScene("Sandbox");
+    }
+
     public void Play()
     {
-
+        SceneManager.LoadScene("Blocagem");
     }
+
     public void config()
     {
         principal.SetActive(false);
-        configuraçao.SetActive(true);
+        configuracao.SetActive(true);
     }
+    
     public void voltar()
     {
         principal.SetActive(true);
-        configuraçao.SetActive(false);
+        configuracao.SetActive(false);
     }
+
     public void voltardopause()
     {
         principal.SetActive(true);
-        configuraçao.SetActive(false);
+        configuracao.SetActive(false);
     }
+
     public void Pause()
     {
         pause.SetActive(false);
         play.SetActive(true);
         principal.SetActive(true);
     }
+    
     public void playpause()
     {
         pause.SetActive(true);
         principal.SetActive(false);
     }
+
     public void sair()
     {
         Application.Quit();
