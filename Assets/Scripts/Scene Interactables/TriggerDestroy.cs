7b13c8d5c5f6aed54d4dd5f489a8b169bdc02798
Túlio Dias <57201326+TTDias@users.noreply.github.com>
2026-05-15 20:03:41 -0300

    finishing 3th puzzle

---
+++ Assets/Scripts/Scene Interactables/TriggerDestroy.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerDestroy : MonoBehaviour
+{
+#nullable enable
+    public List<GameObject> trackingObjs;
+    public Collider[] collisionList;
+
+    public enum Behaviors { Enter, Exit, StayActivate, StayDeactivate}
+    public Behaviors behavior;
+
+    UnityAction<Collider> EnterBehavior = (Collider col) => { return; };
+    UnityAction<Collider> ExitBehavior = (Collider col) => { return; };
+
+    int count = 0;
+
+    void Deactivate(Collider collider)
+    {
+        if (collisionList.Length > 0)
+        {
+            if (trackingObjs.Count > 0)
+            {
+                if (trackingObjs.Contains(collider.gameObject))
+                {
+                    foreach (Collider coll in collisionList)
+                        coll.enabled = false;
+                    return;
+                }
+            }
+            else
+            {
+                foreach (Collider coll in collisionList)
+                    coll.enabled = false;
+            }
+        }
+    }
+    void Activate(Collider collider)
+    {
+        if (collisionList.Length > 0)
+        {
+            if (trackingObjs.Count > 0)
+            {
+                if (trackingObjs.Contains(collider.gameObject))
+                {
+                    foreach (Collider coll in collisionList)
+                        coll.enabled = true;
+                    return;
+                }
+            }
+            else
+            {
+                foreach (Collider coll in collisionList)
+                    coll.enabled = true;
+            }
+        }
+    }
+
+
+    private void Start()
+    {
+        
+        switch (behavior)
+        {
+            case Behaviors.Enter:
+                EnterBehavior = Deactivate;
+                break;
+            case Behaviors.Exit:
+                ExitBehavior = Deactivate;
+                break;
+            case Behaviors.StayActivate:
+                EnterBehavior = Activate;
+                ExitBehavior = Deactivate;
+                break;
+            case Behaviors.StayDeactivate:
+                EnterBehavior = Deactivate;
+                ExitBehavior = Activate;
+                break;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        count --;
+        if(count == 0)
+            ExitBehavior(other);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (count == 0)
+            EnterBehavior(other);
+        count++;
+    }
+}
