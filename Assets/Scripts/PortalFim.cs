11d08beab5d47d742e13e6307197ff6e785c5574
Túlio Dias <57201326+TTDias@users.noreply.github.com>
2026-06-28 22:43:59 -0300

    Bug fix

--- Assets/Scripts/PortalFim.cs
+++ Assets/Scripts/PortalFim.cs
@@ -3,7 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class PortalFim : MonoBehaviour
 {
-    private void OnDestroy()
+    public void NextLevel()
     {
         SceneManager.LoadScene("StoryRobot2");
     }
